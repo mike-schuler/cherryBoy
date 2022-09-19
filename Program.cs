aecdbdfb7fb290a1fd8a7028526356e5a2408727
Mike Schuler <m.schuler133@gmail.com>
2022-09-19 18:50:49 -0400

    Basic Framework outline

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using Microsoft.Extensions.Configuration;
