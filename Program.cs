65b16f0a21d9f01b58e7c001afcc48bc68dc2dba
Mike Schuler <m.schuler133@gmail.com>
2022-09-19 20:07:47 -0400

    Base Config of bot, is able to connect to discord

--- Program.cs
+++ Program.cs
@@ -6,3 +6,17 @@ using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.Interactivity;
 using Microsoft.Extensions.Configuration;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+
+        }
+    }
+}
---
+++ configs.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+
+namespace DiscordBot
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("CommandPrefix")]
+        public string Prefix { get; private set; }
+        [JsonProperty("appSecret")]
+        public string appSecret { get; private set; }
+        [JsonProperty("appId")]
+        public string appID { get; private set; }    
+
+    }
+}
